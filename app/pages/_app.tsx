281e2c1c13f60483114760b64d6253961405e4fa
Ghost Chain <ghost@cashio.app>
2022-03-28 11:31:35 -0400

    remove static image optimization

--- app/pages/_app.tsx
+++ app/pages/_app.tsx
@@ -1,9 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
-import Image from 'next/image'
 import Nav from '../components/Nav'
-import CashioLogo from '../images/cashio.png'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -12,10 +10,14 @@ function MyApp({ Component, pageProps }: AppProps) {
         <Head>
           <title>Cashio Refund</title>
           <link rel="icon" href="/favicon.ico" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
         </Head>
         <div className="mt-5 flex flex-row items-center">
-          <Image src={CashioLogo} height="48px" width="48px" />
+          <img src="/cashio.png" height="48px" width="48px" />
           <p className="ml-2 text-2xl">cashio.app refunds</p>
         </div>
         <Nav />
